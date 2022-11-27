2d764b9b64005e3eb3d0379272188f503e1072e3
Nikolay Zadorojnyy <znikolay69@gmail.com>
2022-11-27 17:07:53 +0300

    add homework_2 task_2_4

---
+++ seminar/homework_2/task_2_4/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую
+// день недели, и проверяет, является ли этот день выходным. Не использовать строки
+// 6 -> да7 -> да1 -> нет   
+int DayOff(int number) // функция определяет день недели
+{
+int result = -1;
+   if (number > 5 && number < 8)
+       {
+         result = 1;
+       }
+       return result;
+}
+int value = new Random().Next(1, 8);             //генерируем число от 1 до 7
+Console.WriteLine($"Исходное число = {value}");
+DayOff(value);
+int condition = DayOff(value);
+if (condition == 1)
+    {
+        Console.WriteLine($"Веденное число {value} - это выходной день");
+    }
+else
+    {
+        Console.WriteLine($"Введенное число {value} - это рабочий день");
+    }
