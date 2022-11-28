519532e647f768ef5fe4aed304cf5cfedf795d78
Nikolay Zadorojnyy <znikolay69@gmail.com>
2022-11-28 17:47:13 +0300

    add lecture less_1

---
+++ lecture/less_1/Task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.Write (" Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша") // переводим все в нижний регистр
+{
+System.Console.WriteLine("Ура, это же МАША");
+}
+else
+{
+System.Console.Write("Привет, ");
+System.Console.WriteLine(username);
+}
