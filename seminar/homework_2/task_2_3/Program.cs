aaac31df43d7ac37fdeda72bcc342b1e45346542
Nikolay Zadorojnyy <znikolay69@gmail.com>
2022-12-02 06:12:13 +0300

    add some task homework_2

--- seminar/homework_2/task_2_3/Program.cs
+++ seminar/homework_2/task_2_3/Program.cs
@@ -2,8 +2,50 @@
 //  удаляет вторую цифру этого числа. Не использовать строки для расчета
 // 456 -> 46782 -> 72918 -> 98
 
-int value = new Random().Next(100,1000);             //генерируем число от 100 до 999
+int value = new Random().Next(100, 1000);             //генерируем число от 100 до 999
 Console.WriteLine($"Исходное число = {value}");
 var str = value.ToString();                           //Превращаем число в строку
-int number = int.Parse(str.Remove(str.Length -2, 1)); //удаляем 1 символ со второй позиции с конца в строке
+int number = int.Parse(str.Remove(str.Length - 2, 1)); //удаляем 1 символ со второй позиции с конца в строке
 Console.WriteLine($"Число после удаления второй цифры = {number}");
+
+// еще вариант
+
+int num = new Random().Next(100, 1000);             //генерируем число от 100 до 999
+Console.WriteLine(num);
+num = (num % 10) + (num / (10 * 10)) * 10; // так как переменная int - то дробная часть выкинется
+Console.WriteLine(num);
+
+// Напишите программу, которая выводит случайное трёхзначное число и
+//  показывает вторую цифру этого числа.
+
+int num1 = new Random().Next(100, 1000);             //генерируем число от 100 до 999
+Console.WriteLine(num1);
+num1 %= 100; //num1 = num1%100 ;
+num1 /= 10;  // num1 = num1 / 10;
+Console.WriteLine(num1);
+
+// Напишите программу, которая выводит случайное  число и
+//  показывает третью цифру этого числа или сообщает что ее нет.
+
+int num2 = new Random().Next();             //генерируем любое число
+Console.WriteLine($"Исходное число {num2}");
+Value3(num2);
+int Value3(int num3) //функция определения третьей цифры числа
+{
+if (num3 < 100 && num3 > -100)return -1;
+    else
+    {
+        while (!(num3 / 1000 == 0))
+        {
+            num3 /= 10;
+        }
+        num3 %= 10;
+    }
+    return (num3 >= 0 ? num3 : -num3);
+}
+if (Value3(num2) == -1)
+{
+System.Console.WriteLine("У этого числа нет третьей цифры ");
+}
+else
+System.Console.WriteLine($"Третья цифра числа {num2} это {Value3(num2)}");
--- seminar/homework_2/task_2_4/Program.cs
+++ seminar/homework_2/task_2_4/Program.cs
@@ -3,22 +3,56 @@
 // 6 -> да7 -> да1 -> нет   
 int DayOff(int number) // функция определяет день недели
 {
-int result = -1;
-   if (number > 5 && number < 8)
-       {
-         result = 1;
-       }
-       return result;
+    int result = -1;
+    if (number > 5 && number < 8)
+    {
+        result = 1;
+    }
+    return result;
 }
 int value = new Random().Next(1, 8);             //генерируем число от 1 до 7
 Console.WriteLine($"Исходное число = {value}");
 DayOff(value);
 int condition = DayOff(value);
 if (condition == 1)
-    {
-        Console.WriteLine($"Веденное число {value} - это выходной день");
-    }
+{
+    Console.WriteLine($"Веденное число {value} - это выходной день");
+}
 else
-    {
-        Console.WriteLine($"Введенное число {value} - это рабочий день");
-    }
+{
+    Console.WriteLine($"Введенное число {value} - это рабочий день");
+}
+
+// еще решение через switch
+
+System.Console.WriteLine("Ведите день недели ");
+int num = Convert.ToInt32(Console.ReadLine());
+switch (num)
+{
+    case 6:
+    case 7:
+        Console.WriteLine("День является выходным");
+        break;
+        default:
+        System.Console.WriteLine("День не является выходным");
+        break;
+}
+
+// еще решение чеоез else if
+
+System.Console.WriteLine("Ведите день недели ");
+int number = Convert.ToInt32(Console.ReadLine());
+int day = number;
+if (day >= 1 && day <= 5)
+{
+    System.Console.WriteLine("День является рабочим ");
+}
+else if (day == 6 || day == 7)
+{
+    System.Console.WriteLine("День является выходным ");
+}
+else
+{
+    System.Console.WriteLine("Некорректное число ");
+}
+
