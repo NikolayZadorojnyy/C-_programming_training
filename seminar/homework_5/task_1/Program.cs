c1dbe9a24e95a5ec83378db40a3dcc194cabfbdd
Nikolay Zadorojnyy <znikolay69@gmail.com>
2022-12-05 04:48:29 +0300

    add seminar homework_5

---
+++ seminar/homework_5/task_1/Program.cs
@@ -0,0 +1,50 @@
+﻿// **Задача 1**: Задайте массив заполненный случайными положительными трёхзначными 
+// числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] GenerateArray(int Length, int minRange, int maxRange)
+{
+    int[] array = new int[Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(minRange, maxRange);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write(array[j] + " ");
+    }
+    Console.WriteLine();
+}
+
+int Promt(String message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// функция считает количество четных чисел
+int EvenNumber(int[] Array)
+{
+    int count = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i] %2 == 0)
+        {
+          count = count +1;
+        }
+    }
+    return count;
+}
+
+int Length = Promt("Введите длину массива ");
+int minArray = 100;
+int maxArray = 1000;
+
+int[] array = GenerateArray(Length, minArray, maxArray);
+PrintArray(array);
+System.Console.WriteLine($"Количество четных чисел в массиве = {EvenNumber(array)}");
---
+++ seminar/homework_5/task_2/Program.cs
@@ -0,0 +1,48 @@
+﻿// **Задача 2:** Задайте одномерный массив, заполненный случайными 
+// числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] GenerateArray(int Length, int minRange, int maxRange)
+{
+    int[] array = new int[Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(minRange, maxRange);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write(array[j] + " ");
+    }
+    Console.WriteLine();
+}
+
+int Promt(String message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// метод находит сумму элементов на нечетных позициях
+int SummOddIndex(int[] Array)
+{
+    int sum = 0;
+    for (int i = 0; i < Array.Length; i += 2)
+    {
+        sum += Array[i];
+    }
+    return sum;
+}
+
+int Length = Promt("Введите длину массива ");
+int minArray = Promt("Введите минимальное значение ");
+int maxArray = Promt("Введите максимальное значение ");
+
+int[] array = GenerateArray(Length, minArray, maxArray);
+PrintArray(array);
+System.Console.WriteLine($"Сумма элементов на нечётных позициях = {SummOddIndex(array)}");
