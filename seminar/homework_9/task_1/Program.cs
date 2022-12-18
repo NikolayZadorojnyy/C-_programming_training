6b1b800500818e9bce41266f8c2b068248cbd224
Nikolay Zadorojnyy <znikolay69@gmail.com>
2022-12-19 01:34:43 +0300

    add seminar homework_9

---
+++ seminar/homework_9/task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// **Задача 1:** Задайте значения M и N. Напишите программу, которая
+// выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"M = 4; N = 8 -> "4, 6, 8"
+
+// ввод данных
+int Prompt(string message)
+{
+    Console.Write($"{message} > ");              // Вывод приглашения
+    return Convert.ToInt32(Console.ReadLine()); // ввод числа
+}
+
+// рекурсивный метод нахождения целых четных чисел в диапазоне
+void PrintEvenNumber(int numberStart, int numberFinish)
+{
+    if (numberStart == numberFinish)
+        return;
+    if (numberStart % 2 == 0)
+        System.Console.Write($"{numberStart} ");
+    PrintEvenNumber(numberStart + 1, numberFinish); // рекурсивный вызов метода
+}
+
+int numberStart = Prompt("Введите число M "); ;
+int numberFinish = Prompt("Введите число N "); ;
+System.Console.WriteLine("Ряд целых натуральных чисел, расположенных между M и N : ");
+PrintEvenNumber(numberStart, numberFinish);
---
+++ seminar/homework_9/task_3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 3: Напишите программу вычисления функции Аккермана с
+// помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// ввод данных
+int Prompt(string message)
+{
+    Console.Write($"{message} > ");              // Вывод приглашения
+    return Convert.ToInt32(Console.ReadLine());  // ввод числа
+}
+
+// рекурсивный метод рассчета функции Аккермана
+int FunkAkkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else
+      if ((m != 0) && (n == 0))
+        return FunkAkkerman(m - 1, 1);
+    else
+        return FunkAkkerman(m - 1, FunkAkkerman(m, n - 1));
+}
+
+int numberStart = Prompt("Введите число M ");
+int numberFinish = Prompt("Введите число N ");
+System.Console.Write("Функция Аккермана равна: ");
+Console.WriteLine(FunkAkkerman(numberStart, numberFinish));
+
+
