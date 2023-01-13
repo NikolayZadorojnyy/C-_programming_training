d212b419a3498b9ab8a3171cd1650c5c20f27507
Nikolay Zadorojnyy <znikolay69@gmail.com>
2023-01-13 05:05:26 +0300

    add Bootcamp_Programming less_3 task_2

---
+++ Bootcamp_Programming/less_3/task_2/Program.cs
@@ -0,0 +1,35 @@
+﻿// алгоритм таблица умножения стандартным способом
+// сложность выполнения O(n^2)
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+  for (int j = 1; j <= n; j++)
+  {
+    System.Console.Write(i * j);
+    System.Console.Write("\t");
+  }
+  System.Console.WriteLine();
+}
+
+// алгоритм таблица умножения выполняется в два раза быстрее  
+// за счет заполнения только половины матрицы буквой г
+// сложность выполнения O(n^2/2)
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m, m];
+for (int i = 0; i < m; i++)
+{
+  for (int j = 0; j < m; j++)
+  {
+    matrix[i, j] = (i + 1) * (j + 1);
+    matrix[j, i] = (i + 1) * (j + 1);
+  }
+}
+for (int i = 0; i < m; i++)
+{
+  for (int j = 0; j < m; j++)
+  {
+    System.Console.Write(matrix[i, j]);
+    System.Console.Write("\t");
+  }
+  System.Console.WriteLine();
+}
