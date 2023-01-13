470640716c5b8308c2c6fe6e3b97f6a69d3bfd71
Nikolay Zadorojnyy <znikolay69@gmail.com>
2023-01-13 05:04:12 +0300

    add Bootcamp_Programming less_3 task_1

---
+++ Bootcamp_Programming/less_3/task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Сложность работы алгоритма - O(n) (5 операций)
+
+int n = 5;
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+  array[i] = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("[" + string.Join(" ", array) + "]");
+int summa = 0;
+for (int i = 0; i < n; i++)
+  summa += array[i];  // сумма всех элементов массива
+System.Console.WriteLine(summa);
