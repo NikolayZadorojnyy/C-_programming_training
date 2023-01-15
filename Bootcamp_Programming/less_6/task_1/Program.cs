97a7037b7d2d987bc86c53cec949d1dcccfa66bc
Nikolay Zadorojnyy <znikolay69@gmail.com>
2023-01-15 17:35:03 +0300

    мелкие исправления

--- Bootcamp_Programming/less_6/task_1/Program.cs
+++ Bootcamp_Programming/less_6/task_1/Program.cs
@@ -8,6 +8,7 @@
 int[] arr = { 0, -5, 2, 3, 5, 9, -1, 7 };
 QuickSort(arr, 0, arr.Length - 1);
 Console.Write($"Отсортированный массив {string.Join(", ", arr)}");
+Console.ReadLine();
 
 static void QuickSort(int[] inputArray, int minIndex, int maxIndex)
 {
